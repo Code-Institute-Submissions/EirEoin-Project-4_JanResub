54ac8827da56e9d524381ceb16860eaca0503365
EirEoin <eoinc2010@live.ie>
2022-10-04 11:23:18 +0000

    trying to change database url) git push

--- blog/models.py
+++ blog/models.py
@@ -2,23 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft",)(1, "Published"))
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post (models.Model):
     title= models.CharField(max_length=200, unique=True)
-    slug = models.SlugFielf(max_length=200, unique=True)
-    author =models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     update_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    feautured_image = CloudianryField('image', default='placeholder')
+    feautured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='blogpost_likes', blank=True)
 
     class Meta:
-        ordering = [ '-Created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
--- dailygalway/settings.py
+++ dailygalway/settings.py
@@ -42,9 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage'
-    'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary_storage',
+    'cloudinary',
     'blog',
 ]
 
@@ -80,7 +79,7 @@ WSGI_APPLICATION = 'dailygalway.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/databases
 
 # DATABASES = {
 #     'default': {
@@ -89,8 +88,9 @@ WSGI_APPLICATION = 'dailygalway.wsgi.application'
 #     }
 #  }
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(),
 }
 
 # Password validation
@@ -134,7 +134,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL ='/media/'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
