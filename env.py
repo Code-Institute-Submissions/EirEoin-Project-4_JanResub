e43fa4268a5e7f4bf4e9618ff2172c1f3eecdaf9
EirEoin <eoinc2010@live.ie>
2023-01-13 15:00:27 +0000

    created env_vars file

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "emotionalboys2012"
-os.environ["CLOUDINARY_URL"] = "cloudinary://158735866257145:mkndpfgnwUMrstfPriqMSailmfQ@dlufzlwnu"
-os.environ["DATABASE_URL"] = "postgres://kecmrjcjhuskzi:72228b5c5bba2bfec4a96529dfb1bd87fc342e3450f2e2c296a2d486048a226b@ec2-54-155-110-181.eu-west-1.compute.amazonaws.com:5432/d5npd9cv18fiho"
-os.environ["PORT"] = "8000"
-os.environ["DISABLE_COLLECTSTATIC"] = "1"
